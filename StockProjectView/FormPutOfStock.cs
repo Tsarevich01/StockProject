41cd9dbfa4ddb13c3f78774ad55008fdfb27e529
Tsarevich01 <pytinvovan@mail.ru>
2019-06-17 00:28:28 +0400

    add

--- StockProjectView/FormPutOfStock.cs
+++ StockProjectView/FormPutOfStock.cs
@@ -2,12 +2,6 @@
 using StockProjectDAL.ViewModel;
 using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using Unity;
 
@@ -41,24 +35,8 @@ namespace StockProjectView
                 MessageBox.Show("Выберите поставщика", "Ошибка", MessageBoxButtons.OK,
                 MessageBoxIcon.Error);
                 return;
-            }
-            try
-            {
-                serviceC.Stock(new OrderBindingModel
-                {
-                    ClientId = Convert.ToInt32(comboBoxClient.SelectedValue),
-                    SnackId = Convert.ToInt32(comboBoxProduct.SelectedValue),
-                    Count = Convert.ToInt32(textBoxCount.Text),
-                    Sum = Convert.ToInt32(textBoxSum.Text)
-                });
-                MessageBox.Show("Сохранение прошло успешно", "Сообщение", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                DialogResult = DialogResult.OK;
-                Close();
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }
+            }          
+            
         }
 
         private void buttonCancel_Click(object sender, EventArgs e)
@@ -107,6 +85,14 @@ namespace StockProjectView
                     dataGridView1.Columns[1].Visible = false;
                     dataGridView1.Columns[2].Visible = false;
                     dataGridView1.Columns[3].Visible = false;
+                }
+                List<ContractorViewModel> list = serviceC.GetList();
+                if (list != null)
+                {
+                    comboBoxContr.DisplayMember = "Названеие";
+                    comboBoxContr.ValueMember = "Id";
+                    comboBoxContr.DataSource = list;
+                    comboBoxContr.SelectedItem = null;
                 }
             }
             catch (Exception ex)
